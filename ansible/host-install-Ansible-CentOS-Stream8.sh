6b50e878d24a5604fb5cedd1070ae160d840a894
Yakov Kolyada <yakov.kolyada@gmail.com>
2023-01-04 14:12:58 +0200

    bash: install Ansible 2.13.5 on host

---
+++ ansible/host-install-Ansible-CentOS-Stream8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dnf install -y epel-release
+dnf install -y ansible
+absible --version
--- bash/host-install-Python3-CentOS-Stream8.sh
+++ bash/host-install-Python3-CentOS-Stream8.sh
@@ -2,7 +2,9 @@
 
 dnf install -y epel-release
 dnf install -y python39
-# alternatives --remove-all python3
-# alternatives --install /usr/bin/python3 python3 $(which python3.9) 111
-# alternatives --set python3 $(which python3.9)
-alternatives --set python $(which python3.9)
+alternatives --remove-all python
+alternatives --install /usr/bin/python python /usr/bin/python3.9 111 --family python
+# alternatives --set python /usr/bin/python3.9
+# alternatives --config python <<< '1'
+alternatives --list | grep python
+python --version
