61666a8757657b7e4ec348673a97abaa01d72160
Yakov Kolyada <yakov.kolyada@gmail.com>
2023-01-04 22:52:10 +0200

    bash: install Ansible 7.1.0 on host

--- ansible/host-install-Ansible-CentOS-Stream8.sh
+++ ansible/host-install-Ansible-CentOS-Stream8.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 dnf install -y epel-release
-dnf install -y ansible
+python3 -m ensurepip --upgrade
+pip install --upgrade pip
+# dnf install -y ansible
+# python3 -m pip install --user ansible-core==2.14.1
+python3 -m pip install --upgrade --user ansible
+
 absible --version
 ansible-config view
